aed7db817e9ea7ccfb408d5a7b1f04c91de8538a
Anton Ushakov <sibens@mail.ru>
2023-12-20 14:16:23 +0100

    first commentary

---
+++ code/xproject/tests/CalorieImplTest.java
@@ -0,0 +1,13 @@
+package xproject.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CalorieImplTest {
+
+    @org.junit.jupiter.api.Test
+    void readProductsFromCSV() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void saveFood() {
+    }
+}
